4744add5ab867d10d690d014346338d73ac2962a
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-04-03 23:58:17 -0400

    Initial Commit

---
+++ ProductScanner/ProductScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductScannerApp.swift
+//  ProductScanner
+//
+//  Created by Timur on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
