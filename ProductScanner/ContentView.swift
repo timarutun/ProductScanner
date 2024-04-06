beadddcf88e22d777924429deed80eea6332891c
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-04-06 02:03:19 -0400

    Added image and nutrients to Product.swift and conected to UI

--- ProductScanner/ContentView.swift
+++ ProductScanner/ContentView.swift
@@ -16,36 +16,49 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            Form{
-                Section(header: Text("Product name")){
-                    Text("\(foundProducts?.title ?? "Title")")
-                        .font(.title2)
-                        .fontWeight(.semibold)
+                Form{
+                    Section {
+                        AsyncImage(url: URL(string: foundProducts?.image ?? "")) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(Rectangle())
+                        } placeholder: {
+                            Rectangle()
+                                .foregroundStyle(.background)
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                    Section(header: Text("Product name")){
+                        HStack {
+                            Text("\(foundProducts?.title ?? "Title")")
+                                .font(.title2)
+                            .fontWeight(.semibold)
+                            
+                        }
+                    }
+                    Section(header: Text("Important Badges")){
+                        Text(foundProducts?.importantBadges.joined(separator:", ") ?? "Badges")
+                            .font(.callout)
+                    }
+                    Section(header: Text("Nutrients")){
+                        Text("Fat: \(String(foundProducts?.nutrition?.fat ?? "0"))")
+                        Text("Protein: \(String(foundProducts?.nutrition?.protein ?? "0"))")
+                        Text("Calories: \(String(foundProducts?.nutrition?.calories ?? 0))")
+                        Text("Carbs: \(String(foundProducts?.nutrition?.carbs ?? "0"))")
+                            .font(.callout)
+                    }
                 }
-                Section(header: Text("Important Badges")){
-                    Text(foundProducts?.importantBadges.joined(separator:", ") ?? "Badges")
-                        .font(.callout)
+                .navigationBarTitle("Poduct Info")
+                .navigationBarItems(trailing:
+                                        Button(action: {
+                    self.isPresented.toggle()
+                }) {
+                    Image(systemName: "barcode")
+                }.sheet(isPresented: $isPresented) {
+                    BarCodeScanner(upc: $upc, foundProducts: $foundProducts)
                 }
-                Section(header: Text("Nutrients")){
-                    Text("Fat:")
-                    Text("Protein:")
-                    Text("Calories:")
-                    Text("Carbohydrates:")
-                        .font(.callout)
-                }
-            }
-            
-            .navigationBarTitle("Poduct Info")
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                self.isPresented.toggle()
-            }) {
-                Image(systemName: "barcode")
-            }.sheet(isPresented: $isPresented) {
-                BarCodeScanner(upc: $upc, foundProducts: $foundProducts)
-            }
             )
-
         }
     }
     
--- ProductScanner/Product.swift
+++ ProductScanner/Product.swift
@@ -8,7 +8,30 @@
 import Foundation
 
 struct Products: Decodable {
-    let title: String
+    let title: String?
     let importantBadges: [String]
+    let nutrition: Nutrition?
+    let image: String?
+}
+
+// MARK: - Nutrition
+struct Nutrition: Codable {
+    let nutrients: [Nutrient]
+    let caloricBreakdown: CaloricBreakdown?
+    let calories: Double?
+    let fat, protein, carbs: String?
+}
+
+// MARK: - Nutrient
+struct Nutrient: Codable {
+    let name: String?
+    let amount: Double?
+    let unit: String?
+    let percentOfDailyNeeds: Double?
+}
+
+// MARK: - CaloricBreakdown
+struct CaloricBreakdown: Codable {
+    let percentProtein, percentFat, percentCarbs: Double?
 }
 
