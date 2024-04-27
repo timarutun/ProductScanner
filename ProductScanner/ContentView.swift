132fe010bc75bfaa290eb1661b5c27133c729eb4
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-04-27 02:17:16 -0400

    UI changes. No more Form

--- ProductScanner/ContentView.swift
+++ ProductScanner/ContentView.swift
@@ -9,110 +9,129 @@ import SwiftUI
 import Charts
 
 struct ContentView: View {
-    
     @State private var showingProductPhoto = false
-    
     @State private var isPresented = false
     @State private var upc: String?
     @State private var foundProducts: Products?
     
     var body: some View {
-        NavigationView{
-                Form{
-                    Section(header: Text("Product name")){
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    if let product = foundProducts {
                         HStack {
-                            Text("\(foundProducts?.title ?? "Title")")
-                                .font(.title2)
-                                .fontWeight(.semibold)
+                            Text(product.title ?? "Title")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
                             
-                        }
-                    }
-                    Section {
-                       
-                        Button(action: {
-                                showingProductPhoto.toggle()
-                            }) {
-                                HStack {
-                                    Text(showingProductPhoto ? "Hide Product Photo" : "Show Product Photo")
-                                    Spacer()
-                                    Image(systemName: (showingProductPhoto ? "chevron.down" : "chevron.up"))
+                            Spacer()
+                            
+                            if let imageURL = URL(string: product.image ?? "") {
+                                AsyncImage(url: imageURL) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .frame(width: 100, height: 100)
+                                } placeholder: {
+                                    Rectangle()
+                                        .foregroundColor(.gray)
+                                        .frame(width: 100, height: 100)
                                 }
-                                .foregroundStyle(.foreground)
+                                .padding()
                             }
-
-                                        if showingProductPhoto {
-                                            AsyncImage(url: URL(string: foundProducts?.image ?? "")) { image in
-                                                image
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .clipShape(Rectangle())
-                                            } placeholder: {
-                                                Rectangle()
-                                                    .foregroundStyle(.background)
-                                            }
-                                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                        }
-                    }
-                    
-                    Section(header: Text("Nutrients")){
-                        let breackdown: [DataForChart] = [
-                            .init(name: "Fat", value: foundProducts?.nutrition?.caloricBreakdown?.percentFat ?? 30, color: .orange),
-                            .init(name: "Protein", value: foundProducts?.nutrition?.caloricBreakdown?.percentProtein ?? 30, color: .blue),
-                            .init(name: "Carbs", value: foundProducts?.nutrition?.caloricBreakdown?.percentCarbs ?? 30, color: .purple)
-                        ]
-                        
-                         Chart(breackdown) { breackdown in
-                            SectorMark(
-                                angle: .value(Text(verbatim: breackdown.id), breackdown.value), innerRadius: .ratio(0.5), angularInset: 5
-                            )
-                            .foregroundStyle(breackdown.color)
-                             
                         }
-                         .frame(height: 300)
-                            
-                        Text("Fat: \(String(foundProducts?.nutrition?.caloricBreakdown?.percentFat ?? 0))")
-                            .foregroundStyle(.orange)
-                        Text("Protein: \(String(foundProducts?.nutrition?.caloricBreakdown?.percentProtein ?? 0))")
-                            .foregroundStyle(.blue)
-                        Text("Carbs: \(String(foundProducts?.nutrition?.caloricBreakdown?.percentCarbs ?? 0))")
-                            .font(.callout)
-                            .foregroundStyle(.purple)
-                        Text("Calories: \(String(foundProducts?.nutrition?.calories ?? 0))")
-                            .fontWeight(.bold)
-                    }
-                    
-                    Section(header: Text("Ingredients")){
-                        if foundProducts == nil {
-                                                Text("Loading...")
-                                            } else {
-                                                ForEach(foundProducts?.ingredients ?? [], id: \.name) { ingredient in
-                                                    VStack(alignment: .leading) {
-                                                        Text(ingredient.name)
-                                                        if let safetyLevel = ingredient.safetyLevel {
-                                                            Text("Safety Level: \(safetyLevel)")
-                                                                .foregroundColor(safetyLevel == "high" ? .green : .red)
-                                                        } else {
-                                                            Text("Safety Level: Unknown")
-                                                                .foregroundColor(.gray)
-                                                        }
-                                                    }
-                                                }
-                                            }
+                        
+                        Section(header: Text("Nutrients").font(.footnote).foregroundStyle(.gray)) {
+                            if let nutrition = product.nutrition {
+                                let breakdown: [DataForChart] = [
+                                    .init(name: "Fat", value: nutrition.caloricBreakdown?.percentFat ?? 0, color: .orange),
+                                    .init(name: "Protein", value: nutrition.caloricBreakdown?.percentProtein ?? 0, color: .blue),
+                                    .init(name: "Carbs", value: nutrition.caloricBreakdown?.percentCarbs ?? 0, color: .purple)
+                                ]
+                                
+                                Chart(breakdown) { data in
+                                    SectorMark(
+                                        angle: .value(Text(data.name), data.value), innerRadius: .ratio(0.5), angularInset: 5
+                                    )
+                                    .foregroundStyle(data.color)
+                                }
+                                .frame(height: 300)
+                                
+                                Text("Fat: \(nutrition.caloricBreakdown?.percentFat ?? 0)")
+                                    .foregroundStyle(.orange)
+                                Text("Protein: \(nutrition.caloricBreakdown?.percentProtein ?? 0)")
+                                    .foregroundStyle(.blue)
+                                Text("Carbs: \(nutrition.caloricBreakdown?.percentCarbs ?? 0)")
+                                    .font(.callout)
+                                    .foregroundStyle(.purple)
+                                Text("Calories: \(nutrition.calories ?? 0)")
+                                    .fontWeight(.bold)
+                            } else {
+                                Text("Nutrition information not available")
+                            }
+                        }
+                        .padding(.bottom, 15)
+                        .fontWeight(.bold)
+                        
+                        Divider()
+                        
+                        Section(header: Text("Ingredients").font(.footnote).foregroundStyle(.gray)) {
+                            if let ingredients = foundProducts?.ingredients {
+                                ForEach(ingredients, id: \.name) { ingredient in
+                                    VStack(alignment: .leading) {
+                                        Text(ingredient.name)
+                                            .foregroundColor(.primary)
+                                        if let safetyLevel = ingredient.safetyLevel {
+                                            Text("Safety Level: \(safetyLevel)")
+                                                .foregroundColor(safetyLevel == "high" ? .green : .red)
+                                                .font(.subheadline)
+                                        } else {
+                                            Text("Safety Level: Unknown")
+                                                .foregroundColor(.gray)
+                                                .font(.subheadline)
+                                        }
+                                    }
+                                    .padding(.vertical, 4)
+                                }
+                            } else {
+                                VStack {
+                                    Image(systemName: "info.circle")
+                                        .font(.system(size: 36))
+                                        .foregroundColor(.gray)
+                                    Text("Ingredients not available")
+                                        .font(.headline)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                        }
+                        
+                    } else {
+                        Text("Loading...")
+                            .padding()
                     }
                 }
-                .navigationBarTitle("Poduct Info")
-                .navigationBarItems(trailing:
-                                        Button(action: {
+                
+            }
+            .padding()
+            .navigationBarTitle("Product Info")
+            .navigationBarItems(trailing:
+                Button(action: {
                     self.isPresented.toggle()
                 }) {
                     Image(systemName: "barcode")
-                }.sheet(isPresented: $isPresented) {
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+                .sheet(isPresented: $isPresented) {
                     BarCodeScanner(upc: $upc, foundProducts: $foundProducts)
                 }
-                )
-
+            )
         }
-        
+        .ignoresSafeArea()
     }
 }
 
@@ -123,10 +142,8 @@ struct DataForChart: Identifiable {
     let color: Color
 }
 
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
